037541efa42ad4b13a170f33f845b98154a5dd15
Gabriela Arrupes <103344390+gabrielarrupes@users.noreply.github.com>
2022-05-21 21:08:17 -0300

    First commit - Operações booleanas.
    Definindo as variáveis da atividade.

---
+++ Day1.js
@@ -0,0 +1,15 @@
+  
+   // definição das variáveis
+
+      let numeroUm = 1;
+      let stringUm = '1';
+      let numeroTrinta = 30;
+      let stringTrinta = '30';
+      let numeroDez = 10;
+      let stringDez = '10';
+
+   
+    
+   
+
+      
