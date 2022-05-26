0107fe8fbc85211e95e77d36f59e9080ced18d85
Gabriela Arrupes <arrupesdev@gmail.com>
2022-05-26 19:33:24 -0300

    Condições das variáveis um - Operações Booleanas

--- Day1.js
+++ Day1.js
@@ -8,7 +8,20 @@
       let numeroDez = 10;
       let stringDez = '10';
 
-   
+  // Condições das variáveis um.
+
+   if (numeroUm == stringUm && numeroUm !== stringUm ) {
+
+   console.log('As variáveis numeroUm e stringUm tem o mesmo valor, mas tipos diferentes.')
+}
+   else if (numeroUm === stringUm) {
+
+   console.log('As variáveis numeroUm e stringUm tem o mesmo valor e tipo.')
+}
+   else {
+      
+   console.log('As variáveis numeroUm e stringUm não tem o mesmo valor.')
+} 
     
    
 
